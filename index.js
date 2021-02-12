8a522d6250ba382b2310ca9f755f8261c8ee1777
MYRIKMY <simvol801@mail.ru>
2021-02-12 19:55:08 +0300

    quest B

---
+++ index.js
@@ -0,0 +1,17 @@
+let booleanElement = false;
+let nullElement = null;
+let undefinedElement;
+let numberElement = 55;
+let stringElement = 'some text';
+let arrayElement = ['cat', 'meow', 'kitten'];
+let objectElement = {name:'cat', tail:true, legs:4};
+let symbolElement = Symbol('id');
+
+alert(booleanElement);
+alert(nullElement);
+alert(undefinedElement);
+alert(numberElement);
+alert(stringElement);
+alert(arrayElement);
+
+console.log(objectElement);
